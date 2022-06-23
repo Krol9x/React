79e1d5889435440efab43b580266000f477306a3
Krol9x <99504658+Krol9x@users.noreply.github.com>
2022-06-23 14:41:49 +0200

    Update StudentView.js

--- src/components/StudentView/StudentView.js
+++ src/components/StudentView/StudentView.js
@@ -1,42 +1,84 @@
 import "./StudentView.css"
 import React from "react";
 import { useDB } from "../../contexts/DBContext";
+import { useState } from "react";
 
 
 export default function StudentView() {
 
-  const {user, getGroupInfo, getSubmitions, addTask, submitSolution,uploadState, getUserGroups, getUserTasks}  = useDB()
+    const { user, getUserTasks, submitSolution } = useDB()
+    const [filename, setfile] = useState("Wybierz plik z komputera")
+    console.log("jestem se tu")
 
-  const logUserInfo =async () => {
-    getUserTasks(user.uid).then(tasks=>{
-      console.log(tasks)
+    //pokaz taski z bazy
+    const logUserInfo = async () => { return
+        console.log("aaa jestem se tu")
+        getUserTasks(user.uid).then(tasks => {
+            console.log("jestem se tu")
+            let listItems = (tasks.map((task) => {
+                return (
+                    <li>
+                        {console.log("xdddd")}
+                        <div className="post">
+                            <div className="header">
+                                <h1>{task.name}</h1>
+                            </div>
+                            <div>
+                                <div className="description"><p>{task.description}</p>
+                                    <h3></h3>
+                                </div>
+                                <div className="attachments">
+                                    <div className="files-container">
 
-    
-      tasks.forEach(task => {
-        console.log(task)
-      })
-      
-    })
-  }
+                                        <ol>
+                                            <li> {filename} <button type="submit" className="add" onClick={dfn} >Usun</button></li>
+                                        </ol>
 
-  const formHandler=(e)=>{
-    e.preventDefault()
-    console.log(e);
-    const file = e.target[0].files[0]
-    submitSolution("testGroup","testTask",file)
-  }
+                                        <div className="buttons">
 
-  
+                                            <form onSubmit={formHandler}>
+                                                <div class="field upload">
+                                                    <label>Wybierz plik z komputera</label>
+                                                    <input type="file" id="upload-button" onChange={changefilename} name="upload-file"></input>
+                                                </div>
+                                                <button type="submit" className="send" >Przeslij Zadanie</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                )
+            }
+            )
+            )
+        }
+        )
+    }
 
-  return (
-    <div>
-      <button onClick={logUserInfo}>log user</button>
-      <form onSubmit={formHandler}>
-        <input type="file" name="file" />
-        <button type="submit">Submit</button>
-      </form>
-      <span class="white">{uploadState}%</span>
+    const formHandler = (e) => {
+        e.preventDefault()
+        console.log("xdddd");
+        const file = e.target[0].files[0]
+        submitSolution("testGroup", "testTask", file)
+    }
 
-    </div>
-  );
+    //dodaje nazwe pliku do tabelki
+    function changefilename(e) {
+        setfile(e.target.files[0].name);
+    }
+    //usuwa nazwe pliku
+    function dfn() {
+        console.log("123");
+        setfile("Wybierz plik z komputera");
+    }
+
+    return (
+        <div>
+            <ul>
+                {logUserInfo}
+            </ul>
+        </div>
+    );
 }
